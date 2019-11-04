ed4a0361841870dbcc050ea24b25c8a0631271e6
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-04 13:36:16 -0800

    Create Pokegame and move Pokedex logic into it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <p>Pokegame</p>
+      <Pokegame />
     </div>
   );
 }

2c0c1e6f42dba560280b47c05ed76128206fb15f
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-04 06:18:29 -0800

    Add background and font styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,9 +5,8 @@ class Pokecard extends Component {
   render() {
 
     return (
-        <div>
-          <p>Pokecard</p>
-          <p>{this.props.name}</p>
+        <div className="Pokecard">
+          <p className="Pokecard-name">{this.props.name}</p>
           <img className="Pokecard-img" src={this.props.img} />
           <p>Type: {this.props.type}</p>
           <p>EXP: {this.props.exp}</p>

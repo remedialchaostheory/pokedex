60d74d3503eb49f38d9d739c06b53bd5dd803005
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-03 16:52:20 -0800

    Add each Pokemon's info and images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class Pokecard extends Component {
   render() {
+
     return (
         <div>
           <p>Pokecard</p>
+          <div>{this.props.name}</div>
+          <div><img src={this.props.img} /></div>
+          <div>Type: {this.props.type}</div>
+          <div>EXP: {this.props.exp}</div>
         </div>
     );
   }
